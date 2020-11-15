481acaaec3bf70efe64c979df7324d1856a6f203
Nassim Essaidi <Contact@essaidi.me>
2020-11-16 00:57:35 +0100

    Remove unnecessary code and optimize the html output

--- extract_wifi_password_windows.py
+++ extract_wifi_password_windows.py
@@ -1,5 +1,8 @@
 import subprocess
 import pandas as pd
+from time import sleep
+import os
+
 
 command1 = ['netsh', 'wlan', 'show', 'profile']
 data = subprocess.check_output(command1).decode('utf-8').splitlines()
@@ -15,8 +18,43 @@ for wifi in wifi_list:
     results = [line.strip() for line in results]
     password_list.append(results[0])
 
-production = {'Wifi Name': [line for line in wifi_list], 'Wifi Password': [line for line in password_list]}
+production = {'ID': [i + 1 for i in range(len(wifi_list))], 'Wifi Name': [line for line in wifi_list],
+              'Wifi Password': [line for line in password_list]}
 
 df = pd.DataFrame(production)
+print()
 print(df)
 
+with open('html_template.txt', 'r') as f:
+    html = f.read()
+
+while True:
+    try:
+        x = input('\nDo you want to save the output? [yes / no]: ')
+        title = 'Wifi_Passwords'
+        if x.casefold() == 'yes'.casefold():
+            # Create Html Page
+            final_title = title + '.html'
+            with open(final_title, 'w') as f:
+                f.write(html.format(table=df.to_html(classes=('table', 'table-hover', 'table-dark'), index=False)))
+
+            df.to_excel(title + '.xlsx', index=False)
+            print(f'The file has been saved successfully under name: {final_title}')
+            sleep(3)
+            print(f'Opening "{final_title}" in 3 seconds...')
+            sleep(1)
+            for i in range(3, 0, -1):
+                sleep(1)
+                print(i)
+            sleep(2)
+            os.system(final_title)
+            break
+
+        elif x.casefold() == 'no'.casefold():
+            print('Have a good day! ^-^')
+
+            break
+        else:
+            print('Wrong choice Try again')
+    except Exception as e:
+        print('Error!', e)
